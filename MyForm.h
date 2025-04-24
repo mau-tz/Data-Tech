6e0f020fa1be4e8c7c45ec0f20280b1030c625c4
ZxAlvaroXz <alvaro.rocha1@hotmail.com>
2025-04-24 16:13:59 -0500

    menu

--- MyForm.h
+++ MyForm.h
@@ -37,6 +37,7 @@ namespace Datatech {
 	private: System::Windows::Forms::Button^ button1;
 	protected:
 	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Label^ label1;
 
 	private:
 		/// <summary>
@@ -53,41 +54,63 @@ namespace Datatech {
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(335, 148);
+			this->button1->Location = System::Drawing::Point(360, 154);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(116, 83);
+			this->button1->Size = System::Drawing::Size(146, 105);
 			this->button1->TabIndex = 0;
-			this->button1->Text = L"Agregar cantidad de empleados (60-120)";
+			this->button1->Text = L"Agregar cantidad de empleado (60 a 120)";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(335, 237);
+			this->button2->Location = System::Drawing::Point(360, 289);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(116, 77);
+			this->button2->Size = System::Drawing::Size(146, 105);
 			this->button2->TabIndex = 1;
 			this->button2->Text = L"Salir del programa";
 			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->ForeColor = System::Drawing::SystemColors::ControlText;
+			this->label1->Location = System::Drawing::Point(399, 93);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(58, 13);
+			this->label1->TabIndex = 2;
+			this->label1->Text = L"Data-Tech";
+			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(867, 605);
+			this->Controls->Add(this->label1);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
 	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	
+	//empezar el programa
+	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	//salir del programa
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
 	};
 }
